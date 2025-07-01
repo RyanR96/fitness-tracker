ce89df2492924369a2a531ad9bd2cba10d1b4f29
Ryan <ryanrana_4@hotmail.com>
2025-07-02 00:40:14 +0100

    Created GET for all workouts and :id workouts

--- routes/api/workout.js
+++ routes/api/workout.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const prisma = require("../../client");
+const verifyToken = require("../../middleware/verifyToken");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("workout route");
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const workouts = await prisma.workout.findMany({
+      where: { userId: req.user.id },
+      include: { exercises: true },
+    });
+    res.json(workouts);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.get("/:id", verifyToken, async (req, res) => {
+  try {
+    const workout = await prisma.workout.findFirst({
+      where: { id: parseInt(req.params.id), userId: req.user.id },
+      include: { exercises: true },
+    });
+    res.json(workout);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;

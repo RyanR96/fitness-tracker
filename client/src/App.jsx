e2f51ed052ca9095f4b56058fa508a7b873626f6
Ryan <ryanrana_4@hotmail.com>
2025-07-24 21:04:59 +0100

    Basic protection on dashboard route and added tailwind dependency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -9,6 +11,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        ></Route>
       </Routes>
     </div>
   );
---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute(props) {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  return props.children;
+}
+
+export default ProtectedRoute;

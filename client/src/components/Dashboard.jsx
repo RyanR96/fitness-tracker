0a63c104e6fbad626d478992bfc9cc3e15ca9795
Ryan <ryanrana_4@hotmail.com>
2025-08-02 23:19:45 +0100

    Created and styled the cards for dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,64 @@
-import bg from "../assets/bg.jpg";
-
 function Dashboard() {
   return (
-    <div className="b">
-      <div className="a" style={{ backgroundImage: `url(${bg})` }} />
+    <div className="min-h-screen flex flex-col">
+      {" "}
+      {/* flex flex-col and flex-1 later on ensures is 100% height */}
+      <div
+        className="h-[50vh] bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center text-white"
+        style={{
+          backgroundImage: `url(https://cdn.pixabay.com/photo/2016/01/08/01/53/gymer-1126999_960_720.jpg)`,
+        }}
+      >
+        <div>
+          <h1>HELLOHELLOHELLOHELLOHELLOHELLOHELLO</h1>
+        </div>
+      </div>
+      <div className=" bg-gray-900 text-white p-10 flex-1">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
+          {/* Card 1*/}
+          <div className="bg-[#2A2A2A] rounded-xl p-6 text-white text-center">
+            <h3 className="text-xl font-bold mb-1">Create a Workout</h3>
+            <p className="text-sm text-gray-400 mb-4">
+              Create a workout from our list of exercises
+            </p>
+            <button className="bg-green-500 text-black px-6 py 2 rounded-full font-semibold hover:bg-green-300">
+              Create Workout
+            </button>
+          </div>
+          {/* Card 2*/}
+          <div className="bg-[#2A2A2A] rounded-xl p-6 text-white text-center">
+            <h3 className="text-xl font-bold mb-1">Start a Workout</h3>
+            <p className="text-sm text-gray-400 mb-4">
+              Start a workout from a list of your workouts
+            </p>
+            <button className="bg-green-500 text-black px-6 py 2 rounded-full font-semibold hover:bg-green-300">
+              Start Workout
+            </button>
+          </div>
+          {/* Card 3*/}
+
+          <div className="bg-[#2A2A2A] rounded-xl p-6 text-white text-center">
+            <h3 className="text-xl font-bold mb-1">Workout History</h3>
+            <p className="text-sm text-gray-400 mb-4">
+              Check your previous completed workouts
+            </p>
+            <button className="bg-green-500 text-black px-6 py 2 rounded-full font-semibold hover:bg-green-300">
+              Workout history
+            </button>
+          </div>
+
+          {/* Card 3*/}
+          <div className="bg-[#2A2A2A] rounded-xl p-6 text-white text-center">
+            <h3 className="text-xl font-bold mb-1">Track your weight</h3>
+            <p className="text-sm text-gray-400 mb-4">
+              Track your own weight in chart form
+            </p>
+            <button className="bg-green-500 text-black px-6 py 2 rounded-full font-semibold hover:bg-green-300">
+              Track weight
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

e7bafce0577d2a782b9b306c1918628327315323
Ryan <ryanrana_4@hotmail.com>
2025-08-28 17:43:35 +0100

    Added outline of CSS/Tailwind of StartWorkout page, mock data also added

--- client/src/components/StartWorkout.jsx
+++ client/src/components/StartWorkout.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react";
+
 function StartWorkout() {
+  const workout = {
+    id: 1,
+    name: "Push Day",
+    exercises: [
+      {
+        exerciseTemplateName: "Push Ups",
+        sets: [{ weight: "", reps: "", formRating: "", dropSet: false }],
+      },
+      {
+        exerciseTemplateName: "Bench Press",
+        sets: [{ weight: "", reps: "", formRating: "", dropSet: false }],
+      },
+    ],
+  };
+
+  const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
+  const [exercise, setExercise] = useState(workout.exercises);
+
+  const currentExercise = exercise[currentExerciseIndex];
+
   return (
-    <>
-      <div>Start Workout</div>
-    </>
+    <div className="min-h-screen bg-gray-50 p-6 flex flex-col">
+      <h1 className="text 3x1 font-bold mb-2 text-center">
+        Workout name - change to dynamic later
+      </h1>
+      <p className="text-center mb-4">Exercise 1 of 3 - change to dynamic</p>
+
+      <div className="flex-1 flex flex-col md:flex-row gap-6">
+        <div className="flex-1 bg-gray-200 p-6 rounded shadow">
+          <h2 className="text-2x1 font-semibold mb-4 text-center">
+            {currentExercise.exerciseTemplateName}
+          </h2>
+        </div>
+      </div>
+    </div>
   );
 }
 

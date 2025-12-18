9450f868a88917ff8d860328b024a30642e37f71
Ryan <ryanrana_4@hotmail.com>
2025-12-18 17:28:47 +0000

    Adjusted some text size. Added a scrollbar when there's loads of sets in an exercise

--- client/src/components/CompletedWorkoutHistory.jsx
+++ client/src/components/CompletedWorkoutHistory.jsx
@@ -51,17 +51,20 @@ function CompletedWorkoutHistory() {
 
   return (
     <div className="min-h-screen bg-gray-50 p-6 flex flex-col">
-      <h1 className="text 3x1 font-bold mb-2 text-center">Workout History</h1>
-      <div className="flex-1 flex flex-col md:flex-row gap-6  border-2 border-dashed border-blue-500 ">
+      <h1 className="text-lg font-bold mb-2 text-center">Workout History</h1>
+      <div className="flex-1 flex flex-col md:flex-row gap-6">
         <div className="flex-1 bg-gray-200 p-6 rounded shadow">
-          <h2 className="text-2x1 font-semibold mb-4 text-center">
+          <h2 className="text-base font-semibold mb-4 text-center">
             {completedWorkout.workout?.name || "Deleted Workout"}
             <br></br>
             {new Date(completedWorkout.date).toLocaleDateString()}
           </h2>
 
           {completedWorkout.exercises.map(exercise => (
-            <div key={exercise.id} className="mb-6">
+            <div
+              key={exercise.id}
+              className="mb-6 max-h-80 overflow-y-auto custom-scrollbar"
+            >
               <h2 className="text-lg font-semibold mb-2 text-center p-5">
                 {exercise.exerciseTemplateName}:
               </h2>
@@ -69,8 +72,9 @@ function CompletedWorkoutHistory() {
                 {exercise.set.map((set, i) => (
                   <li
                     key={i}
-                    className="grid grid-cols-2 sm:grid-cols-4 text-center items-center bg-gray-100 px-2 "
+                    className="grid grid-cols-2 sm:grid-cols-4 text-center items-center bg-gray-100 px-2 mr-6 "
                   >
+                    {/**gap-1 mb-1 items-center bg-white px-2 rounded-xl shadow-sm border border-gray-200  Either this or the one above, both look nice imo*/}
                     <span className="p-2 ">
                       <strong>Set {i + 1}:</strong>
                     </span>
